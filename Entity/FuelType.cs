1f448adc2593cec2c317eb8028be7e9774784140
Anar Khalilov <anar_x@yahoo.com>
2021-01-22 22:21:12 +0300

    Add files via upload

---
+++ Entity/FuelType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern.Entity
+{
+    public enum FuelType
+    {
+        Any,
+        Gasoline,
+        Diesel,
+        Electric
+    }
+}
