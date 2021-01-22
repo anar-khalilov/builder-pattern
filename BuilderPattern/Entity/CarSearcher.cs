5ede1e62808a795fbeaae5994d8fbfb3ffc16ded
Anar Khalilov <anar_x@yahoo.com>
2021-01-22 22:20:05 +0300

    Add files via upload

---
+++ BuilderPattern/Entity/CarSearcher.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern.Entity
+{
+    class CarSearcher
+    {
+        internal static List<Car> Search(CarSearchCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
