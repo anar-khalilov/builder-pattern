70a405fa54b1cd49814605578f42c028de77f391
Anar Khalilov <anar_x@yahoo.com>
2021-01-22 22:15:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BuilderPattern.Entity;
+
+namespace BuilderPattern
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //CarSearchCriteria criteria = new CarSearchCriteria(BodyStyle.Hatchback, FuelType.Diesel, null, null, null, 2014, 2019, null, null, null, null);
+
+            CarSearchCriteria criteria = new CarSearchCriteriaBuilder().HavingBodyStyle(BodyStyle.Hatchback)
+                                                                       .HavingFuelType(FuelType.Diesel)
+                                                                       .ProducedInOrAfterYear(2014)
+                                                                       .ProducedInOrBeforeYear(2019)
+                                                                       .Build();
+
+            List<Car> results = CarSearcher.Search(criteria);
+
+            // Display results...
+        }
+    }
+}
